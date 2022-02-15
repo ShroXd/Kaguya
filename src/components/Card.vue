f1970ec8ad02adba4604d977ac3e5eea0a7a006f
ShroXd <ShroXdF@gmail.com>
2022-02-15 23:38:10 +0800

    feat: 🎸 add rotateX for card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,23 @@
 <template>
-  <div
-    class="py-8 px-6 border-1 border-light-800 shadow hover:shadow-xl ease-in duration-150 w-auto inline-block"
-  >
-    <slot />
+  <div class="container">
+    <div
+      class="animate py-8 px-6 border-1 border-light-800 shadow hover:(shadow-xl) ease-in duration-150 w-auto inline-block"
+    >
+      <slot />
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.container {
+  perspective: 1000px;
+}
+
+.animate {
+  transform-style: preserve-3d;
+
+  &:hover {
+    transform: rotateX(20deg);
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,56 @@
 <script setup lang="ts">
 import Card from '~/components/Card.vue'
+
+interface Kana {
+  hiragana: string
+  katakana: string
+  romaji: string
+}
+
+// const data: Kana[] = [{
+//   hiragana: 'あ',
+//   katakana: 'ア',
+//   romaji: 'a',
+// }, {
+//   hiragana: 'い',
+//   katakana: 'イ',
+//   romaji: 'i',
+// }, {
+//   hiragana: 'う',
+//   katakana: 'ウ',
+//   romaji: 'u',
+// }, {
+//   hiragana: 'え',
+//   katakana: 'エ',
+//   romaji: 'e',
+// }, {
+//   hiragana: 'お',
+//   katakana: 'オ',
+//   romaji: 'o',
+// }]
+
+const data: Kana[] = [{
+  hiragana: 'あ',
+  katakana: 'ア',
+  romaji: 'a',
+}]
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-constraint
+// const pick = <T extends unknown>(array: T[]) => array[Math.floor(Math.random() * array.length)]
 </script>
 
 <template>
-  <card class="cursor-pointer">
+  <card v-for="k, idx in data" :key="idx" class="cursor-pointer m-8">
     <div class="text-12xl">
-      あ
+      {{ k.hiragana }}
     </div>
     <hr class="my-6">
     <div class="flex justify-around">
       <div class="text-6xl">
-        ア
+        {{ k.katakana }}
       </div>
       <div class="text-6xl">
-        a
+        {{ k.romaji }}
       </div>
     </div>
   </card>
